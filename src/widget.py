278146c6f1d0bed87c4c083ddb611d4f6572737c
Frinny <Toxa.Vol@yandex.ru>
2024-08-05 10:48:32 +0300

    design corrections

--- src/widget.py
+++ src/widget.py
@@ -1,18 +1,18 @@
 def mask_account_card(number: str) -> str:
     """Returns masked card or account number as string"""
-    if 'Счет' in number:
-        return f'{number[0:5]}{'*' * 2}{number[-4::]}'
+    if "Счет" in number:
+        return f"{number[0:5]}{'*' * 2}{number[-4::]}"
     else:
         card_name = []
         for symbol in number:
-            if symbol not in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
+            if symbol not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
                 card_name.append(symbol)
             else:
                 break
-        card_name_str = ''.join(card_name)
-        return f'{card_name_str}{number[-16:-12]} {number[-12:-10]}{'*' * 2} {'*' * 4} {number[-4::]}'
+        card_name_str = "".join(card_name)
+        return f"{card_name_str}{number[-16:-12]} {number[-12:-10]}{'*' * 2} {'*' * 4} {number[-4::]}"
 
 
 def get_date(date: str) -> str:
     """Returns common date write as string"""
-    return f'{date[8:10]}.{date[5:7]}.{date[0:4]}'
+    return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
